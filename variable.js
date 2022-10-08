a1977dbafa0d319c9822e2e425253837ebfeea4f
Satwik Anand Chauhan <93284505+satwikanandchauhan@users.noreply.github.com>
2022-10-08 12:58:17 +0530

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,11 @@
+// You can also use let keyword in place of var after es6....
+
+let a = 10;
+let b = 20;
+let c = a + b;
+let d = a - b;
+let e = a * b;
+let f = a / b;
+let g = a % b;
+
+console.log(c, d, e, f, g);
